b3e88febc4718bfe21384a73227be462ec0a386f
Iskra Vitaly <iskra.vitaly@gmail.com>
2018-01-29 21:31:33 +0300

    Removed verbose log statements

--- remove-lj-ads.user.js
+++ remove-lj-ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         remove-lj-ads
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        http://*.livejournal.com/*
@@ -12,10 +12,8 @@
 (function() {
     'use strict';
     let mo = window.MutationObserver || window.WebKitMutationObserver;
-//    console.log('window.MutationObserver=', mo);
     var observer = new mo(function(mutations) {
         for (let m of mutations) {
-//            console.log('mutation', m);
             if (m.type === 'childList') {
                 for (let n of m.addedNodes) {
                     if (n.id && /^begun_block/.exec(n.id)) {
